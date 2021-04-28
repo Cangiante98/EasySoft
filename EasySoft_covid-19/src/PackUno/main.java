1f5db6ac5c59ee8a02eccc9dba605005353dc5d5
De De giglio Marco <marco_degiglio@libero.it>
2021-04-28 19:03:53 +0200

    modifica di prova

--- EasySoft_covid-19/src/PackUno/main.java
+++ EasySoft_covid-19/src/PackUno/main.java
@@ -1,8 +1,12 @@
 package PackUno;
 
 public class main {
-
+	public String bho ="bho";
+	
 	public main() {
+		for (int i=0; i<100; i++) {
+			System.out.println("ok");
+		}
 		// TODO Auto-generated constructor stub
 	}
 
