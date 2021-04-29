45f08ccacf9f4b7a680dba9a46d0cf5b610967bb
ETTORE <e.trisciuzzi2@studenti.uniba.it>
2021-04-29 18:46:04 +0200

    classe persona classe medico

--- EasySoft_covid-19/src/utente/Medico.java
+++ EasySoft_covid-19/src/utente/Medico.java
@@ -1,9 +1,57 @@
 package utente;
 
-public class Medico {
-
-	public Medico() {
-		// TODO Auto-generated constructor stub
+public class Medico{
+	private String nome;
+	private String cognome;
+	private String email;
+	
+	//Costruttori
+	public Medico(){
+		this.nome = "NESSUN NOME";
+		this.cognome = "NESSUN COGNOME";
+		this.email = "NESSUN EMAIL";
 	}
-
+	
+	public Medico(String nomeIniziale, String cognomeIniziale, String emailIniziale) {
+		this.nome = nomeIniziale;
+		this.cognome = cognomeIniziale;
+		this.email = emailIniziale;
+		
+	}
+	
+	
+	//Reimposta dati medico 	
+	public void setMedico(String nome, String cognome, String email) {
+		this.nome = nome;
+		this.cognome = cognome;
+		this.email = email;
+	}
+	
+	public void setNome(String nuovoNome) {
+		this.nome = nuovoNome;
+	}
+	
+	public void setCognome(String nuovoCognome) {
+		this.nome = nuovoCognome;
+	}
+	
+	public void setEmail(String nuovaEmail) {
+		this.email = nuovaEmail;
+	}
+	
+	
+	//Ottieni dati medico
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public String getCognome() {
+		return this.cognome;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
 }
+
--- EasySoft_covid-19/src/utente/Persona.java
+++ EasySoft_covid-19/src/utente/Persona.java
@@ -1,32 +1,54 @@
 package utente;
 
-public class Persona {
+public class Persona{
 	private String nome;
 	private String cognome;
 	private String codiceFiscale;
 	private String telefono;
 	
+	//Costruttori
 	public Persona(){
 		this.nome = "NESSUN NOME";
 		this.cognome = "NESSUN COGNOME";
 		this.codiceFiscale = "0000000000";
-		this.telefono = "0000000000";
+		this.telefono = "+39" + "0000000000";
 	}
 	
 	public Persona(String nomeIniziale, String cognomeIniziale, String codiceFiscaleIniziale, String telefonoIniziale) {
 		this.nome = nomeIniziale;
 		this.cognome = cognomeIniziale;
 		this.codiceFiscale = codiceFiscaleIniziale;
-		this.telefono = telefonoIniziale;
+		this.telefono = "+39" + telefonoIniziale;
 	}
 	
+	
+	//Reimposta dati persona 	
 	public void setPersona(String nome, String cognome, String codiceFiscale, String telefono) {
 		this.nome = nome;
 		this.cognome = cognome;
 		this.codiceFiscale = codiceFiscale;
-		this.telefono = telefono;
+		this.telefono = "+39" + telefono;
 	}
 	
+	public void setNome(String nuovoNome) {
+		this.nome = nuovoNome;
+	}
+	
+	public void setCognome(String nuovoCognome) {
+		this.nome = nuovoCognome;
+	}
+	
+	public void setCodFiscale(String nuovoCodFiscale) {
+		this.nome = nuovoCodFiscale;
+	}
+	
+	public void setTelefono(String nuovoTelefono) {
+		this.nome = nuovoTelefono;
+	}
+	
+	
+	
+	//Ottieni dati persona
 	public String getNome() {
 		return this.nome;
 	}
@@ -42,5 +64,7 @@ public class Persona {
 	public String getTelefono() {
 		return this.telefono;
 	}
+	
+	
 }
 
