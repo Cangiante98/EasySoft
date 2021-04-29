274441e5b6066422faaa0b589f1a496d9d47f953
De giglio Marco <79160832+DegiglioMarco98@users.noreply.github.com>
2021-04-29 16:22:24 +0200

    prova

--- EasySoft_covid-19/src/PackUno/main.java
+++ EasySoft_covid-19/src/PackUno/main.java
@@ -2,16 +2,12 @@ package PackUno;
 
 public class main {
 	public String bho ="bho";
-	
-	public main() {
-		for (int i=0; i<100; i++) {
-			System.out.println("ok");
-		}
-		// TODO Auto-generated constructor stub
-	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		for (int i=0; i<100; i++) {
+			System.out.println("ok");
+		}
 
 	}
 
